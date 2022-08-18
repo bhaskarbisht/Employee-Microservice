f4624a054e4a8b1d40c61c8a6a694254538bc6db
unknown <cogjava3121@iiht.tech>
2022-08-18 12:54:47 +0530

    added restTemplate

--- src/main/java/com/empl/EmployeeMicroserviceApplication.java
+++ src/main/java/com/empl/EmployeeMicroserviceApplication.java
@@ -2,6 +2,8 @@ package com.empl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmployeeMicroserviceApplication {
@@ -10,4 +12,8 @@ public class EmployeeMicroserviceApplication {
 		SpringApplication.run(EmployeeMicroserviceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- src/main/java/com/empl/controller/EmployeeController.java
+++ src/main/java/com/empl/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.empl.controller;
 
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.empl.model.Employee;
 import com.empl.service.IEmployeeService;
@@ -16,10 +19,15 @@ public class EmployeeController {
 	@Autowired
 	private IEmployeeService employeeservice;
 	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@GetMapping("/{eid}")
 	public Employee getEmployee(@PathVariable("eid") Long eid) {
 		Employee employee=employeeservice.getEmployee(eid);
+		
+		List records= this.restTemplate.getForObject("http://localhost:8090/record/emp/"+eid, List.class);
+		employee.setRecords(records);
 		return employee;
 	}
 }
